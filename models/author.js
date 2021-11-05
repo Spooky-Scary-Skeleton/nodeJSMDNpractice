fe9e2a3332ac2cda026e10945ebcf2ffb15fb82d
Dan Kim <ddad9583@gmail.com>
2021-11-05 17:52:20 +0900

    test

--- models/author.js
+++ models/author.js
@@ -1,36 +1,35 @@
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
-let AuthorSchema = new Schema(
-  {
-    first_name: {type: String, required: true, maxlength: 100},
-    family_name: {type: String, required: true, maxlength: 100},
-    date_of_birth: {type: Date},
-    date_of_death: {type: Date},
-  }
-);
-
-AuthorSchema
-.virtual('name')
-.get(function () {
-  return this.family_name + ', ' + this.first_name;
+let AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxlength: 100 },
+  family_name: { type: String, required: true, maxlength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
 });
 
-AuthorSchema.virtual('lifespan').get(function() {
-  let lifetime_string = '';
-  if(this.date_of_birth) {
-    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
-  }
-  lifetime_string += ' - ';
-  if(this.date_of_death) {
-    lifetime_string = DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
-  }
-  return lifetime_string
-})
-
-AuthorSchema
-.virtual('url')
-.get(function () {
-  return '/catalog/author' + this._id;
+AuthorSchema.virtual("name").get(function () {
+  return this.family_name + ", " + this.first_name;
 });
 
-module.exports = mongoose.model('Author', AuthorSchema);
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime_string = "";
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  lifetime_string += " - ";
+  if (this.date_of_death) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  return lifetime_string;
+});
+
+AuthorSchema.virtual("url").get(function () {
+  return "/catalog/author" + this._id;
+});
+
+//test
+module.exports = mongoose.model("Author", AuthorSchema);

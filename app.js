6d4894faa4da0a2bc46a2eb32e53a5fbf111627d
Dan Kim <ddad9583@gmail.com>
2021-11-08 07:41:04 +0900

    test

--- app.js
+++ app.js
@@ -1,48 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-let mongoose = require('mongoose');
-let mongoDB = 'mongodb+srv://2875:2875@cluster0.qbmk9.mongodb.net/local_library?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+let mongoose = require("mongoose");
+let mongoDB =
+  "mongodb+srv://1111:1111@cluster0.qbmk9.mongodb.net/local_library?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection Error'));
+db.on("error", console.error.bind(console, "MongoDB connection Error"));
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const cataloguRouter = require('./routes/catalog');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const cataloguRouter = require("./routes/catalog");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog', cataloguRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/catalog", cataloguRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
